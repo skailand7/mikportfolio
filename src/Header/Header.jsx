db66e25ef02af6580a04372309d7946dd2693b07
Miguel Turpo <miganguelturpo@gmail.com>
2022-02-11 10:43:02 -0500

    [ADD] media queries

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import menu from "../menu.svg";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <img
-          src="http://cdn.onlinewebfonts.com/svg/img_565446.png"
-          alt="Brackets"
-        />
-        <p>
-          Mik<span>Code</span>
-        </p>
+        <div className="logo-text">
+          <img
+            src="http://cdn.onlinewebfonts.com/svg/img_565446.png"
+            alt="Brackets"
+          />
+          <p>
+            Mik<span>Code</span>
+          </p>
+        </div>
+        <img className="logo-menu" src={menu}></img>
       </div>
       <nav>
         <ul>
